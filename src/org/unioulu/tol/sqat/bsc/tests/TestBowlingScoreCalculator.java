390444bd36482dce65cd864ea88978cb79a3f289
somename <someemail>
2015-10-16 12:46:58 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -11,10 +11,18 @@ public class TestBowlingScoreCalculator {
 	public void test() {
 		
 		BowlingGame bg = new BowlingGame();
+		BowlingGame bg2 = new BowlingGame();
+		BowlingGame bg3 = new BowlingGame();
+		BowlingGame bg4 = new BowlingGame();
 		
 		bg.setBonus(1, 5);
-		
-		assertEquals(false, bg.isNextFrameBonus() );
+           assertEquals(false, bg.isNextFrameBonus() );
+        bg2.setBonus(2, 8);
+           assertEquals(true, bg.isNextFrameBonus() );
+        bg3.setBonus(10, 0);
+           assertEquals(true, bg.isNextFrameBonus() );
+        bg4.setBonus(0, 10);
+           assertEquals(false, bg.isNextFrameBonus() );
 		
 		
 		
