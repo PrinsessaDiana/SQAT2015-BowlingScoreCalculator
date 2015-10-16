0d1cc735880becd80861ba9f829d831c31672376
somename <someemail>
2015-10-16 12:19:08 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/BowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/BowlingGame.java
@@ -7,12 +7,17 @@ public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
+	int BonusPoint;
+	boolean spare;
+	boolean strike;
 	
 	public BowlingGame(){}
 	
 	public void addFrame(Frame frame){
 		
-
+     Frame frame1, frame2, frame3, frame4,frame5,frame6,frame7,frame8, frame9, frame10;
+	
+     
 		
 		
 		
@@ -20,13 +25,23 @@ public class BowlingGame {
 	
 	public void setBonus(int firstThrow, int secondThrow) {
 		
-		int nextRoll;
+		int nextRoll = 0;
 		int BonusPoint;
 		
 		//Bonus points for getting all pins down in two throws
-		if(firstThrow == 1 && secondThrow == 9 ) {
-			BonusPoint = nextRoll;
-				
+		if((firstThrow == 1 && secondThrow == 9) ||
+				(firstThrow == 2 && secondThrow == 8) ||
+				(firstThrow == 3 && secondThrow == 7) ||
+				(firstThrow == 4 && secondThrow == 6) ||
+				(firstThrow == 5 && secondThrow == 5) ||
+				(firstThrow == 6 && secondThrow == 4) ||
+				(firstThrow == 7 && secondThrow == 3) ||
+				(firstThrow == 8 && secondThrow == 2) ||
+				(firstThrow == 9 && secondThrow == 1) ) {
+			
+			//Bonus point is the score of next roll
+			this.BonusPoint = nextRoll;
+			this.spare = true;	
 			
 		}
 			
@@ -41,8 +56,12 @@ public class BowlingGame {
 	
 	public boolean isNextFrameBonus(){
 
-        int bonusPoint;
+        if(spare || strike == true) {
+        	
+        	return true;
+        }
+        else {
 		
-		return false;
+		return false; }
 	}
 }
--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -3,11 +3,19 @@ package org.unioulu.tol.sqat.bsc.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.unioulu.tol.sqat.bsc.BowlingGame;
 
 public class TestBowlingScoreCalculator {
 
 	@Test
 	public void test() {
+		
+		BowlingGame bg = new BowlingGame();
+		
+		bg.setBonus(1, 7);
+		
+		assertEquals(false, bg.isNextFrameBonus() );
+		
 		
 		
 		
